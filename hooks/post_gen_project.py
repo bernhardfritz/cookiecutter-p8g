bdf2cb9e1085c5eaa8a10c3f5b25d6690ed5bbe6
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2022-11-06 21:45:35 +0100

    first commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import subprocess
+import sys
+
+initialized_git = subprocess.call(['git', 'init'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
+if initialized_git:
+    print()
+    print('Initialized a git repository.')
+else:
+    print('Git repo not initialized')
+if subprocess.call(['{{ cookiecutter.package_manager }}', 'install']):
+    sys.exit('`{{ cookiecutter.package_manager }} install` failed')
+if initialized_git:
+    try:
+        subprocess.check_call(['git', 'add', '-A'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        subprocess.check_call(['git', 'commit', '-m', '"Initialize project using cookiecutter-p8g"'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        subprocess.check_call(['git', 'branch', '-M', 'main'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        print()
+        print('Created git commit.')
+    except:
+        print('Git commit not created')
+        print('Removing .git directory...')
+        shutil.rmtree('.git', True)
+print()
+print('Success! Created {{ cookiecutter.project_name }} at {}'.format(
+    os.getcwd()))
+print('Inside that directory, you can run several commands:')
+print()
+print('  {{ cookiecutter.package_manager }} start')
+print('    Starts the development server.')
+print()
+print('  {{ cookiecutter.package_manager }} {}build'.format(
+    '' if '{{ cookiecutter.package_manager }}' == 'yarn' else 'run '))
+print('    Bundles the app into static files for production.')
+print()
+print('We suggest that you begin by typing:')
+print('  cd {{ cookiecutter.project_slug }}')
+print('  {{ cookiecutter.package_manager }} start')
+print()
+print('Happy hacking!')
---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,22 @@
+# {{ cookiecutter.project_name }}
+
+This project was bootstrapped with [cookiecutter-p8g](https://github.com/bernhardfritz/cookiecutter-p8g).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `{{ cookiecutter.package_manager }} start`
+
+Runs the app in the development mode.\
+Open [http://localhost:8080](http://localhost:8080) to view it in your browser.
+
+The page will reload when you make changes.
+
+### `{{ cookiecutter.package_manager }}{% if cookiecutter.package_manager != "yarn" %} run{% endif %} build`
+
+Builds the app for production to the `build` folder.
+
+## Learn More
+
+You can learn more in the [p8g documentation](https://bernhardfritz.github.io/p8g).
